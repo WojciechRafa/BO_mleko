e706d4e03371a16279ff7226cab9c12b8c1faebc
jakubsarata <jakub.sarata@gmail.com>
2022-01-09 09:30:03 +0100

    3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import List
 from typing import Tuple
+import matplotlib.pyplot as plt
 
 import data
 import d_struct
@@ -10,6 +11,8 @@ import target_fun
 max_iter = 50 #maksymalna ilość iteracji
 n = 5 #liczba sprawdzanych kroków w jednej iteracji
 
+#pomocnicze listy do badania przebiegu algorytmu
+values = [] #lista przechowująca kolejne wartości funkcji celu
 
 if __name__ == '__main__':
     R = []  # rozwiązanie jest listą 5-ciu list krotek zawierających obiekt typu node i ilość mleka wlaną/wylaną w danym miejscu
@@ -26,8 +29,20 @@ fun_value, is_legal = target_fun.t_fun(R)
 
 while iter < max_iter:
     #generowanie nowych rozwiązań i wybór najlepszego
-    new_result = steps.get_random_steps(R, n)
-    for ele in new_result:
-        pass
+    steps_list = steps.get_random_steps(R, n)
+    for ele in steps_list:
+        new_R = steps.make_step(R,ele)
+        new_fun_value, is_legal = target_fun.t_fun(new_R)
+        if (is_legal == True) and (new_fun_value >= fun_value):
+            R = new_R
+            fun_value = new_fun_value
+    values.append(fun_value) 
+   
     #obsługa listy tabu
-    #wizualizacja wyników
+
+
+#wizualizacja wyników
+# plt.plot(values)
+# plt.xlabel('iteracja')
+# plt.ylabel('wartość')
+# plt.show()
