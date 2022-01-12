b077f8d1e160c5c20c4a26f57a86789f280f3883
WojciechRafa <wojciechrafa@student.agh.edu.pl>
2022-01-12 12:20:18 +0100

    WPR_12_1_2022_3

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ l_ele = lr + lm + 1 #liczba węzłów grafu
 con_range = (1, 10) #zakres połączeń między wierzchołkami
 
 
-def start_alg():
+def start_alg(iter, max_step, tabu_l, iterator):
     # pomocnicze listy do badania przebiegu algorytmu
     values = []  # lista przechowująca kolejne wartości funkcji celu
     is_acceptable = []  # lista przechowująca kolejne informacje o wykonywalniści funkcji
@@ -39,9 +39,14 @@ def start_alg():
     print('\r')
     
     # tymczasowow zakomneotwane
-    max_iter = int(input("Podaj liczbę iteracji: "))
-    n = int(input("Podaj liczbę kroków sprawdzanych w jednej iteracji: "))
-    TL_dl = int(input("Podaj długość listy tabu: "))
+    #max_iter = int(input("Podaj liczbę iteracji: "))
+    #n = int(input("Podaj liczbę kroków sprawdzanych w jednej iteracji: "))
+    #TL_dl = int(input("Podaj długość listy tabu: "))
+
+    max_iter = iter
+    n = max_step
+    TL_dl = tabu_l
+
     print('\r')
 
     result = steps.get_random_steps(R, 5)
@@ -135,25 +140,37 @@ def start_alg():
     
     print('\r')
     print("Najlepsze otrzymane rozwiązanie:")
+    result:str = ""
     # wyświetlanie wyników
     nr = 0
     for day in best_solution:
         nr += 1
         print('Dzień', nr)
+        result += 'Dzień ' + str(nr) + "\n"
         for w in day:
             print(w[0].name, w[0].nr, '->', w[1])
+            result += str(w[0].name) + str(w[0].nr) + '->' + str(w[1]) + "\n"
         print('\r')
+        result += "\r\n"
     print('Zysk:', max_value)
+    result += "Zysk " + str(max_value) + "\n"
     if acceptable == True:
         print("Wynik jest dopuszczalny")
+        result += "Wynik jest dopuszczalny"
     else:
         print("Wynik jest niedopuszczalny")
+        result += "Wynik jest niedopuszczalny"
+
+    text_file = open("Opis{}.txt".format(iterator), "w")
+    text_file.write(result)
+    text_file.close()
 
     # wizualizacja przebiegu algorytmu
     plt.plot(values)
     plt.xlabel('iteracja')
     plt.ylabel('wartość')
-    plt.show()
+    #plt.show()
+    plt.savefig('Fig{}.png'.format(iterator))
 
 
 if __name__ == '__main__':
@@ -208,10 +225,12 @@ if __name__ == '__main__':
                             [[b, 0], [r[0], 30 ], [r[2], 50 ],  [m[0], 50 ],  [r[2], 60 ]],
                             [[b, 0], [r[1], 100], [r[0], 100],  [m[1], 160],  [m[0], 40 ]]]
 
-    while True:
-        in_str = input("Rozpocząć algorytm [T] - Tak: ")
-        if in_str == 't' or in_str == 'T':
-            start_alg()
+    tested = [50, 10, 20]
+
+    #while True:
+       # in_str = input("Rozpocząć algorytm [T] - Tak: ")
+    for i in range(5):
+        start_alg(tested[0], tested[1], tested[2], i)
 
 
 
